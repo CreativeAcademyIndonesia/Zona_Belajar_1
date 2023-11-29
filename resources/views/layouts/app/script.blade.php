74b4d3254a061aafd486d0069045e0eeb0a6980e
Agus Sugandi <agus.sugandi95@gmail.com>
2023-11-29 21:38:04 +0700

    experimental dashboard

---
+++ resources/views/layouts/app/script.blade.php
@@ -0,0 +1,5 @@
+<script src="{{ asset('assets/js/plugins/chartjs.min.js') }}" async></script>
+<script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}" async></script>
+<script src="{{ asset('assets/js/argon-dashboard-tailwind.js?v=1.0.1') }}" async></script>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
---
+++ resources/views/layouts/app/style.blade.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+@vite('resources/css/app.css')
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+<link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<link href="{{ asset('assets/css/argon-dashboard-tailwind.css?v=1.0.1') }}" rel="stylesheet" />
+<title>@yield('title')</title>
